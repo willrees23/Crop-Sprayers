158c41c75f0b6885118589cb33761e3cf6f950e0
William <64597475+willrees23@users.noreply.github.com>
2026-08-25 17:12:31 +0100

    finish passive particles

--- src/main/java/com/github/willrees23/sprayer/visuals/PassiveParticleVisual.java
+++ src/main/java/com/github/willrees23/sprayer/visuals/PassiveParticleVisual.java
@@ -1,5 +1,6 @@
 package com.github.willrees23.sprayer.visuals;
 
+import com.github.willrees23.CropSprayersPlugin;
 import com.github.willrees23.sprayer.CropSprayer;
 import org.bukkit.Particle;
 import org.bukkit.scheduler.BukkitTask;
@@ -11,7 +12,7 @@ public class PassiveParticleVisual {
     private BukkitTask particleTask;
 
     private static final Particle PARTICLE_TYPE = Particle.FIREWORK;
-    private static final int EMIT_INTERVAL_TICKS = 20;
+    private static final int RATE_TICKS = 5;
 
     public PassiveParticleVisual(CropSprayer sprayer, ArmorStandVisual armorStand) {
         this.sprayer = sprayer;
@@ -19,14 +20,32 @@ public class PassiveParticleVisual {
     }
 
     public void spawn() {
+        if (particleTask != null)
+            return;
 
+        CropSprayersPlugin plugin = CropSprayersPlugin.getInstance();
+        particleTask = plugin.getServer().getScheduler().runTaskTimer(plugin, this::emit, 0L, RATE_TICKS);
     }
 
     public void despawn() {
-        // TODO: cancel particleTask and null it out
+        if (particleTask != null) {
+            particleTask.cancel();
+            particleTask = null;
+        }
     }
 
     private void emit() {
-        // TODO: spawn the passive particles
+        if (armorStand.getStand() == null) {
+            despawn();
+            return;
+        }
+
+        armorStand.getStand().getWorld().spawnParticle(
+                PARTICLE_TYPE,
+                armorStand.getStand().getLocation().add(0, 1, 0),
+                2, // count
+                0.4, 0.1, 0.4, // offsetX, offsetY, offsetZ
+                0.001 // extra
+        );
     }
 }
