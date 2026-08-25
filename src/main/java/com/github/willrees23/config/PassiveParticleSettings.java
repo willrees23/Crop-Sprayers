aeeb5aa89c862109df5832261ff9004c7fadb316
William <64597475+willrees23@users.noreply.github.com>
2026-08-25 21:42:37 +0100

    update default passive particles

--- src/main/java/com/github/willrees23/config/PassiveParticleSettings.java
+++ src/main/java/com/github/willrees23/config/PassiveParticleSettings.java
@@ -13,7 +13,7 @@ public class PassiveParticleSettings {
     private Particle particle = Particle.FIREWORK;
 
     @Comment("Ticks between emissions. 20 ticks = 1 second.")
-    private int rateTicks = 5;
+    private int rateTicks = 7;
 
     @Comment("How many particles to emit each time.")
     private int count = 2;
@@ -23,9 +23,9 @@ public class PassiveParticleSettings {
 
     @Comment("Size of the box the particles are scattered through, in blocks.")
     private double offsetX = 0.4;
-    private double offsetY = 0.1;
+    private double offsetY = -0.1;
     private double offsetZ = 0.4;
 
     @Comment("Particle speed. What this does varies by particle type.")
-    private double extra = 0.001;
+    private double extra = 0.1;
 }
